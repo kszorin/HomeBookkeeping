ebd38133fb4faceb3c5813f56b45cfd4caa9a275
kszorin <kszorin@yandex.ru>
2020-08-25 13:42:57 +0700

    Revert "HB-7 Попробовал android databinding"
    This reverts commit b0307e3c

--- feature-signup/src/main/java/ru/kszorin/homebookkeeping/feature_signup/ui/SignupFragment.kt
+++ feature-signup/src/main/java/ru/kszorin/homebookkeeping/feature_signup/ui/SignupFragment.kt
@@ -4,21 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import ru.kszorin.homebookkeeping.feature_signup.R
-import ru.kszorin.homebookkeeping.feature_signup.databinding.SignupFragmentBinding
-import ru.kszorin.homebookkeeping.feature_signup.presentation.viewmodel.SignupViewModel
 
 class SignupFragment : Fragment() {
 
-    private val viewModel: SignupViewModel by viewModels()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.signup_fragment, container, false)
 
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val dataBinding = DataBindingUtil.inflate<SignupFragmentBinding>(inflater, R.layout.signup_fragment, container, false)
-		dataBinding.setLifecycleOwner(this)
-		dataBinding.bindingViewModel = viewModel
-		return dataBinding.root
-	}
 }
